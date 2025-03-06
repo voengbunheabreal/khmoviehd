2aba0bb0236781636d3b8fbdf12f00a00b9d66c9
theheab <voengbunheab123@gmail.com>
2025-03-07 00:10:01 +0700

    first upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <Navbar />
+    <router-view />
+    <BottomNav />
+  </template>
+  
+  <script>
+    import BottomNav from './components/BottomNav.vue';
+    import Navbar from './components/Navbar.vue';
+    import Home from './components/Home.vue';
+    import Chinese from './components/Chinese.vue';
+    import Thai from './components/Thai.vue';
+    import Khmer from './components/Khmer.vue';
+
+    
+  
+  export default {
+    components: { Navbar, BottomNav , Home , Chinese , Thai , Khmer },
+  };
+  </script>
+  
---
+++ src/components/BottomNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fixed bottom-0 left-0 w-full bg-black text-white p-3 flex justify-around md:hidden">
+    <router-link to="/" class="text-center flex flex-col items-center">
+      <span>🏠</span>
+      <p class="text-xs">ទំព័រដើម</p>
+    </router-link>
+    <router-link to="/chinese" class="text-center flex flex-col items-center">
+      <span>🎬</span>
+      <p class="text-xs">Chinese</p>
+    </router-link>
+    <router-link to="/thai" class="text-center flex flex-col items-center">
+      <span>🎬</span>
+      <p class="text-xs">Thai</p>
+    </router-link>
+    <router-link to="/khmer" class="text-center flex flex-col items-center">
+      <span>🎬</span>
+      <p class="text-xs">Khmer</p>
+    </router-link>
+    <router-link to="/indonesia" class="text-center flex flex-col items-center">
+      <span>🎬</span>
+      <p class="text-xs">Indonesia</p>
+    </router-link>
+    <router-link to="/contact" class="text-center flex flex-col items-center">
+      <span>📞</span>
+      <p class="text-xs">Contact</p>
+    </router-link>
+  </div>
+</template>
