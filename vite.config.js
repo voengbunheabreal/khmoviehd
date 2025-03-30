9c202d7938c104c338be5af8212e8304b991ca68
theheab <voengbunheab123@gmail.com>
2025-03-31 00:25:37 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
+  base: '/khmoviehd/', // Change this to your desired base path
   plugins: [vue()],
   resolve: {
     alias: {
