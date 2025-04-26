987f0e7f5bacff66c24ad3b11e8764d8279939df
theheab <voengbunheab123@gmail.com>
2025-04-26 17:05:28 +0700

    update

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+import { resolve } from 'path';
+
+export default {
+  input: 'src/main.js', // Update this to your actual entry file
+  output: {
+    dir: 'dist',
+    format: 'es'
+  },
+  plugins: []
+};
